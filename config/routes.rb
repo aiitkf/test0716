d41ff157970fddb63f8b66996304e0783e38b4f0
Fujikawa, K <alycia05@rcpt.at>
2019-07-16 12:30:36 +0000

    change route /help

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get '/help'
+  get '/help',  to: 'static_pages#help'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
