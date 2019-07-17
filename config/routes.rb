fb31a3d1d6a53026de9578e33b99c0385e66ea77
Fujikawa, K <alycia05@rcpt.at>
2019-07-17 09:01:07 +0000

    update home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/help',  to: 'static_pages#help'
   post '/result1', to: 'static_pages#tempresult'
+  get 'static_pages#questionset1'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
